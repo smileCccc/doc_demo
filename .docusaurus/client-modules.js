3ad57fca8cfebae970d3190f36faeb3580cc4110
smileCccc <BojtorHenrietta257@gmail.com>
2025-07-29 09:17:42 +0800

    Deploy website - based on c447bc929926a69758a4d45cf1852c9f6b75494b

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("F:\\cnj\\docusaurus\\demo\\demo\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("F:\\cnj\\docusaurus\\demo\\demo\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("F:\\cnj\\docusaurus\\demo\\demo\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("F:\\cnj\\docusaurus\\demo\\demo\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("F:\\cnj\\docusaurus\\demo\\demo\\src\\css\\custom.css"),
-];
